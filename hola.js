fac587229d84f23bb033131335fd3b7e2642a23b
Joce <jocelyn.berenice17@gmail.com>
2024-03-05 01:16:05 -0600

    Ejercicio 1 cambiar nombre con alert para el tema aplicaciones web

---
+++ hola.js
@@ -0,0 +1,7 @@
+//Autor: JERONIMO LOPEZ JOCELYN BERENICE     CH38//
+
+function escribeNombre(){
+    const nombre = prompt("Ingrese su nombre");
+    document.getElementById("Hola").innerHTML= `¡Hola ${nombre}!`;
+}
+
